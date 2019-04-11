f82bfa012cfdca73583b2f33cf816a8c3389dfec
ostlej61 <49528522+ostlej61@users.noreply.github.com>
2019-04-11 10:21:27 -0500

    Initial Upload
    Lab11 - First Upload to GitHub

---
+++ EmployeeDictionary/EmployeeDictionary/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDictionary
+{
+    public class Employee
+    {
+        //variables
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0} {1}, Id: {2}", FirstName, LastName, Id);
+        }
+
+        #region Constructors
+        //default constructor
+        public Employee()
+        {
+
+        }
+
+        //custom constructor
+        public Employee(string inFName, string inLName, int inId)
+        {
+            FirstName = inFName;
+            LastName = inLName;
+            Id = inId;
+        }
+        #endregion
+
+
+    }
+}
---
+++ EmployeeDictionary/EmployeeDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create directory
+            Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
+
+            employees.Add(101, new Employee { FirstName = "James", LastName = "Ostler", Id = 101 });
+            employees.Add(102, new Employee { FirstName = "Gary", LastName = "Smiley", Id = 102 });
+            employees.Add(103, new Employee { FirstName = "Robert", LastName = "Wolf", Id = 103 });
+
+            //get employees
+            Console.WriteLine(employees[101].ToString());
+            Console.WriteLine(employees[102].ToString());
+            Console.WriteLine(employees[103].ToString());
+            Console.ReadLine();
+        }
+    }
+}
